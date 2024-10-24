fe3a392146c3b490edcf19715987f27478396391
vijay <vijay.patel@mobilefirstapplications.com>
2024-10-24 09:59:43 +0530

    change backend link localhost to vercel

--- frontend/src/components/ChatRoom.tsx
+++ frontend/src/components/ChatRoom.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import io, {Socket} from 'socket.io-client';
 
-const socket:Socket = io('http://localhost:3001');
+const socket:Socket = io('https://chatroom-backend-weld.vercel.app');
 
 interface Message {
   msg: string;
@@ -18,10 +18,11 @@ const ChatRoom: React.FC = () => {
 
   useEffect(() => {
     // Store the client's socket ID
+    
     socket.on('connect', () => {
       if(socket.id){
         setSocketId(socket.id);
-        console.log(socketId)
+        console.log(socket.id)
       }
     });
     
@@ -84,7 +85,7 @@ export default ChatRoom;
 // import { useEffect, useState } from 'react';
 // import io from 'socket.io-client';
 
-// const socket = io('http://localhost:3001');
+// const socket = io('https://chatroom-backend-weld.vercel.app');
 
 // const ChatRoom: React.FC = () => {
 //   const [messages, setMessages] = useState<string[]>([]);
